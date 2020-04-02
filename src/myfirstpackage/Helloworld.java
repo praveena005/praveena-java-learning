13fce3fe619eccfed756ed7ee3d81eb0c4f459df
praveena <pravy005@gmail.com>
2020-04-02 17:59:34 +0100

    first commit

---
+++ src/myfirstpackage/Helloworld.java
@@ -0,0 +1,36 @@
+package myfirstpackage;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first Java Program");
+
+	}
+     
+	
+	byte byte_data = 120;
+	
+	short short_data =10500;
+	
+	int int_data = 2355435;
+			
+	long long_data = 435366378;	
+	
+	float float_data = 0.98f;
+	
+	double double_data = 0.98;
+	
+    boolean decision = true;
+    
+    char char_data = 'f';
+    
+    
+    int num1 = 10;
+    int num2 = 30;
+    
+    int sum = num1 + num2;
+    
+    System.out.println("Addition of two numbers = "+sum);
+  
+}
---
+++ src/myfirstpackage/package-info.java
@@ -0,0 +1,9 @@
+package myfirstpackage;
+
+public class hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is my First Java Program");
+	}
+}
